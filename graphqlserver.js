a2ffab1f0e5c5752e2af6fdc810da66241231dfb
Ole Ildsgaard Hougaard <olehougaard@gmail.com>
2022-05-03 08:43:56 +0200

    Connectivity issues persist

--- graphqlserver.js
+++ graphqlserver.js
@@ -4,15 +4,20 @@ const neo4j = require("neo4j-driver")
 const fs = require("fs").promises
 
 const driver = neo4j.driver(
-    "neo4j+s://localhost:7687",
-    neo4j.auth.basic("neo4j", "admin")
+    "bolt://localhost:7687",
+    neo4j.auth.basic("neo4j", "password"), 
+    {encrypted: "ENCRYPTION_OFF"}
 )
+
+driver.verifyConnectivity()
+.then(() => console.log('Verified'))
+.catch(err => console.error(`Error: ${err}`))
 
 fs.readFile('./friends.idl', 'utf8')
 .then(gql)
 .then(typeDefs => new Neo4jGraphQL({ typeDefs, driver }))
 .then(graphQL => graphQL.getSchema())
-//.then(schema => new ApolloServer({schema}))
-//.then(server => server.listen())
-//.then(({ url }) => console.log(`GraphQL server ready on ${url}`))
+.then(schema => new ApolloServer({schema}))
+.then(server => server.listen())
+.then(({ url }) => console.log(`GraphQL server ready on ${url}`))
 .catch(console.err)
