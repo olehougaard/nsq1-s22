d40a7d7962b65bf3a234f3ad9f371c312b6b356f
OIH <OIH@VIA.DK>
2023-04-27 12:19:23 +0200

    upgraded dependencies

--- graphqlserver.js
+++ graphqlserver.js
@@ -3,21 +3,25 @@ const { Neo4jGraphQL } = require("@neo4j/graphql")
 const neo4j = require("neo4j-driver")
 const fs = require("fs").promises
 
+async function startServer(driver) {
+    try {
+        await driver.verifyConnectivity()
+        console.log('Verified')
+        const content = await fs.readFile('./friends.sdl', 'utf8')
+        const typeDefs = gql(content)
+        const graphQL = new Neo4jGraphQL({typeDefs, driver})
+        const schema = await graphQL.getSchema()
+        const server = new ApolloServer({schema})
+        const {url} = await server.listen()
+        console.log(`GraphQL server ready on ${url}`)
+    } catch (err) {
+        console.error(`Error: ${err}`)
+    }
+}
 const driver = neo4j.driver(
-    "bolt://localhost:7687",
+    "bolt://127.0.0.1:7687",
     neo4j.auth.basic("neo4j", "password"), 
     {encrypted: "ENCRYPTION_OFF"}
 )
 
-driver.verifyConnectivity()
-.then(() => console.log('Verified'))
-.catch(err => console.error(`Error: ${err}`))
-
-fs.readFile('./friends.sdl', 'utf8')
-.then(gql)
-.then(typeDefs => new Neo4jGraphQL({ typeDefs, driver }))
-.then(graphQL => graphQL.getSchema())
-.then(schema => new ApolloServer({schema}))
-.then(server => server.listen())
-.then(({ url }) => console.log(`GraphQL server ready on ${url}`))
-.catch(console.err)
+startServer(driver)
